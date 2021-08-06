20989d00ff6df2156a0a9434a242e53059f30bbf
abdulmumeen <abdulmumeen15500@gmail.com>
2021-08-06 19:38:00 +0100

    chore: added bodyparser and mongoose

--- app.js
+++ app.js
@@ -1,6 +1,24 @@
+const dotenv =require("dotenv")
 const express = require("express");
+const bodyParser = require("body-parser");
+const {mongoose} = require("mongoose");
+if (process.env.NODE_ENV !== "production") {
+  dotenv.config({ path: __dirname + "/.env" });
+}
+const MONGO_URI = process.env.MONGO_URI
 
 const app = express();
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended:true}));
+connect(MONGO_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+  })
+  .then(() => console.log("connected to mongodb successfully"))
+  .catch((err) => console.error("could not connect to mongodb", err));
 
 app.use((err,req,res,next)=>{
   if(err){
